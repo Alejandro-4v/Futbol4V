406915d7aa7e0c0be183752189d2bf10b1c0d640
Alejacks <alejandrolopez2048@gmail.com>
2025-10-29 21:59:26 +0100

    fix: Nuevo enlace para nuevo fichero inicio.php en app/

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <nav class="header-nav">
-    <a href="/futbol/">Inicio</a>
+    <a href="/futbol/app/inicio.php">Inicio</a>
     <a href="/futbol/app/equipos.php">Equipos</a>
     <a href="/futbol/app/partidos.php">Partidos</a>
 </nav>
